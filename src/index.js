d1f32bcf8736b9c5c66d013164b392e8f208e79d
osisamkay <osisami.oj@gmail.com>
2019-02-07 22:57:01 +0100

    Client-Project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-require ('./css/index.css');
+ const Toggle = document.querySelector(".imge");
+
+ const Navlink = document.querySelector(".navlink");
+
+ Toggle.addEventListener('click', function () {
+   Navlink.classList.toggle("navlinkshow");
+ })
